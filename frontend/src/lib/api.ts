e112d5b80d1a2b23b14380dbc8a5f0fadd4f6af6
Haniel Rolemberg <victorhanielbusiness@gmail.com>
2026-04-26 18:23:57 -0300

    fix: disable fetch cache and add revalidate to home page to prevent stale blog data

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -23,6 +23,7 @@ async function apiFetch<T>(path: string, options?: RequestInit): Promise<T> {
     const res = await fetch(`${API_URL}${path}`, {
       headers: { 'Content-Type': 'application/json', ...options?.headers },
       signal: controller.signal,
+      cache: 'no-store',
       ...options,
     })
 
